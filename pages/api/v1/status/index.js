195449d33e96ffb92709bda06d2caa88a74814a0
Gustavo Aragão <gstv.mra@gmail.com>
2025-10-28 09:27:18 -0300

    finaliza o endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ message: "OK!" });
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseQueryVersion = await database.query("SHOW server_version;");
+  const databaseQueryMaxConnections = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseQueryConnections = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseVersion = databaseQueryVersion.rows[0].server_version;
+  const databaseMaxConnections =
+    databaseQueryMaxConnections.rows[0].max_connections;
+  const databaseOpenedConnections = databaseQueryConnections.rows[0].count;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnections),
+        connections: databaseOpenedConnections,
+      },
+    },
+  });
 }
 
 export default status;
