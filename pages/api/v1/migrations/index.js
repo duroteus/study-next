0069128f78254c6e0669008458e4c51d7b11e4bd
Gustavo Aragão <gstv.mra@gmail.com>
2025-10-29 11:00:58 -0300

    refatora o endpoint `/api/v1/migrations`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -6,34 +6,41 @@ async function migrations(req, res) {
   const allowedMethods = ["GET", "POST"];
   if (!allowedMethods.includes(req.method)) {
     return res.status(405).json({
-      message: "Método não permitido.",
+      error: `Method ${req.method} not allowed.`,
     });
   }
 
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-    dryRun: true,
-  };
+  let dbClient;
 
-  if (req.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    res.status(200).json(pendingMigrations);
-  }
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+      dryRun: true,
+    };
 
-  if (req.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
+    if (req.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      res.status(200).json(pendingMigrations);
+    }
+
+    if (req.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+      const statusCode = migratedMigrations.length > 0 ? 201 : 200;
+      return res.status(statusCode).json(migratedMigrations);
+    }
+  } catch (e) {
+    console.error(e);
+    throw e;
+  } finally {
     await dbClient.end();
-    const statusCode = migratedMigrations.length > 0 ? 201 : 200;
-    return res.status(statusCode).json(migratedMigrations);
   }
 }
 
