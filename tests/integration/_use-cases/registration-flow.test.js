57177b9a2a3f8edcedb7bc9154a80e1737278c69
Gustavo Aragão <gstv.mra@gmail.com>
2025-11-13 16:38:21 -0300

    fix: adiciona um retorno `null` quando não encontra emails no método `getLastEmail()`

---
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -0,0 +1,62 @@
+import activation from "models/activation.js";
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteAllEmails();
+});
+
+describe("Use case: Registration Flow(all successful)", () => {
+  let createUserResponseBody;
+
+  test("Create user account", async () => {
+    const createUserResponse = await fetch(
+      "http://localhost:3000/api/v1/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "RegistrationFlow",
+          email: "registration@flow.com.br",
+          password: "RegistrationFlow",
+        }),
+      },
+    );
+
+    expect(createUserResponse.status).toBe(201);
+
+    createUserResponseBody = await createUserResponse.json();
+
+    expect(createUserResponseBody).toEqual({
+      id: createUserResponseBody.id,
+      username: "RegistrationFlow",
+      email: "registration@flow.com.br",
+      features: ["read:activation_token"],
+      password: createUserResponseBody.password,
+      created_at: createUserResponseBody.created_at,
+      updated_at: createUserResponseBody.updated_at,
+    });
+  });
+
+  test("Receive activation email", async () => {
+    const lastEmail = await orchestrator.getLastEmail();
+
+    const activationToken = await activation.findOneByUserId(
+      createUserResponseBody.id,
+    );
+
+    expect(lastEmail.sender).toBe("<contato@dev.com.br>");
+    expect(lastEmail.recipients[0]).toBe("<registration@flow.com.br>");
+    expect(lastEmail.subject).toBe("Ative seu cadastro!");
+    expect(lastEmail.text).toContain("RegistrationFlow");
+    expect(lastEmail.text).toContain(activationToken.id);
+  });
+
+  test("Activate account", async () => {});
+  test("Login", async () => {});
+  test("Get user information", async () => {});
+});
