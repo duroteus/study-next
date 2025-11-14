17df260729caf32ba88badbfeb160f47fdaed7b9
Gustavo Aragão <gstv.mra@gmail.com>
2025-11-14 09:49:08 -0300

    feat: adiciona a permissão `read:session` para o endpoint `/api/v1/user`

--- models/activation.js
+++ models/activation.js
@@ -57,7 +57,10 @@ async function activateUser(tokenId) {
 }
 
 async function activateUserByUserId(userId) {
-  const activatedUser = await user.setFeatures(userId, ["create:session"]);
+  const activatedUser = await user.setFeatures(userId, [
+    "create:session",
+    "read:session",
+  ]);
   return activatedUser;
 }
 
